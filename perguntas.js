33df068a4a98e118c08f559df286b357d5650d3d
Laritrap <larissa.trapp@escola.pr.gov.br>
2024-10-09 08:46:49 -0300

    aa

---
+++ perguntas.js
@@ -0,0 +1,119 @@
+criaCartao(
+    'Cultura',
+    'Quando foi lançado o primeiro filme da disney?',
+    'Foi no ano 1937'
+)
+
+criaCartao(
+    'Cultura',
+    'Qual cantora tem mais grammys?',
+    'Beyonce'
+)
+
+criaCartao(
+    'História',
+    'Que ano a familia real veio ao Brasil?',
+    'Foi no ano 1808'
+)
+
+criaCartao(
+    'História',
+    'Quem foi o primeiro presidente do Brasil?',
+    'marechal Manuel Deodoro da Fonseca'
+)
+
+criaCartao(
+    'Português',
+    'Qual a principal função da charge?',
+    'Criticar um acontecimento do cotidiano'
+)
+
+criaCartao(
+    'Quimica',
+    'Hidrocarbonetos são formador por apenas',
+    'Carbonos e Hidrogênios'
+)
+
+criaCartao(
+    'Geografia',
+    'Quanto continentes existem no mundo e qual são seus nomes?',
+    '6, Asia, Africa, America, Oceania, Europa e Antartica'
+)
+
+criaCartao(
+    'Biologia',
+    'Quem é considerado o pai da gentica?',
+    'Gregor Mendel'
+)
+
+criaCartao(
+    'Português',
+    'O romantismo teve quantas fases?',
+    '3 fases'
+)
+
+criaCartao(
+    'Biologia',
+    'Quantos ossos existem no corpo adulto de um ser humano?',
+    '206'
+)
+
+criaCartao(
+    'Biologia',
+    'Qual a chance de uma mãe heterozigota ter um/a filho/a heteroigota?',
+    '50%'
+)
+
+criaCartao(
+    'Geografia',
+    'Qual era a doutrina politica durante o absolutismo?',
+    'Mercantilismo'
+)
+
+criaCartao(
+    'Quimica',
+    'Como se calcula a entalpia?',
+    'Entalpia dos Produtos - Entalpia dos regentes'
+)
+
+criaCartao(
+    'Matematica',
+    'Como se calcula o cosseno?',
+    'Cateto adjacente/hipotenusa'
+)
+
+criaCartao(
+    'Programação',
+    'Qual é a linguagem de estilização?',
+    'css'
+)
+
+criaCartao(
+    'Biologia',
+    'qual é o agente vetor da doença de chagas?',
+    'bicho-barbeiro'
+)
+
+criaCartao(
+    'Quimica',
+    'qual é o prefixo da nomenclatura quando se tem 4 carbonos na cadeia principal?',
+    'But'
+)
+
+criaCartao(
+    'História',
+    'Em que ano aconteceu o golpe militar?',
+    'Foi no ano 1964'
+)
+
+criaCartao(
+    'História',
+    'Quantos anos Vargas ficou no poder?',
+    '15 anos'
+)
+
+criaCartao(
+    'Matématica(raciocínio lógico)',
+    'Há um pato entre dois patos, um pato atrás de um pato e um pato na frente de outro pato. De quantos patos estamos falando?',
+    '3 patos'
+)
